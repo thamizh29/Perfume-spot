6470f18027c66e22e37443150b8deaf97fb87baa
thamizh29 <thamizharasan2510@gmail.com>
2025-06-20 23:44:29 +0530

    CI: Data base created ui was added

---
+++ perfume/perfume/doctype/cart/test_cart.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, perfume spot team and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCart(FrappeTestCase):
+	pass
